c487d0f0ff8861ac21fe840e800bdf84962c3fc0
abushuev <jjjbushjjj@gmail.com>
2018-12-16 23:30:08 +0300

    Change logging logic if -v flag is present logs to console, otherwise to logfile

--- sms-client.py
+++ sms-client.py
@@ -3,6 +3,9 @@ import argparse
 import logging
 import logging.handlers
 from uuid import uuid4
+from suds.client import Client
+
+
 
 LOG_FILENAME = '/tmp/sms.log'
 
@@ -10,12 +13,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
 # 100M per logfile rotate every 2
-handler_logfile = logging.handlers.RotatingFileHandler(
-              LOG_FILENAME, maxBytes=104857600, backupCount=2)
-handler_logfile.setFormatter(formatter)
-handler_logfile.setLevel(logging.INFO)
 
-logger.addHandler(handler_logfile)
 
 def handle_exception(exc_type, exc_value, exc_traceback):
     if issubclass(exc_type, KeyboardInterrupt):
@@ -30,7 +28,9 @@ sys.excepthook = handle_exception
 # subcommand functions
 
 # Send message
-def send(args):
+def send(client, args):
+    """ Try to send message over sms gateway """
+
     logger.debug("Connecting with those params: Wsdl url: %s, "
                   "Sender name: %s, Sender type: %s, User: %s, "
                   "Pass %s, Sending message to: %s With content: %s",
@@ -39,11 +39,20 @@ def send(args):
     # Write messageid to logfile allways level info
     messageId = uuid4()
     logger.info("Sending message with ID: %s To: %s", messageId, args.phone)
+    logger.debug("Client: %s", client)
+    # request_data = client.factory.create('ns0:CapitalCity')
+    # request_data.sCountryISOCode = "533"
+    # result = client.service.CapitalCity('RU')
+    # result = client.service.CountriesUsingCurrency('EUR')
+    result = client.service.FullCountryInfo('RU')
+    result = dict(result)
+    print result['Languages'][0][0]['sName']
 
-    # Sender implementation here
 
 # Get message status
-def get_status(args):
+def get_status(client, args):
+    """ Get status of previously sended message """
+
     logger.debug("Trying to get status for message: %s", args.messageId)
 
     # Getter implementation here
@@ -65,7 +74,7 @@ def main():
                         action="store", type=str)
     parser.add_argument("-w", "--wsdl",
             help="Sms gate wsdl url",
-            action="store_true", default='http://sms-gate/sendmessage.wsdl')
+            action="store", default='http://sms-gate/sendmessage.wsdl')
     
     subparsers = parser.add_subparsers(help='sub-command help')
 
@@ -81,28 +90,36 @@ def main():
     send_parser.add_argument("message",
             help="Message to send", type=str)
     send_parser.add_argument("-s", "--sender",
-            help="Sender name. Displayed in sms header. Must be set according provider rules. Default: Sovcombank",
-                        action="store", default='Sovcombank')
+            help="Sender name. Displayed in sms header. Must be set according provider rules. Default: Sender",
+                        action="store", default='Sender')
     send_parser.add_argument("-t", "--type",
             help="Sender type. Sender type needed for sms gate",
-                        action="store", default='some type')
+                        action="store", default='senderType')
     send_parser.set_defaults(func=send)
 
 
     args = parser.parse_args()
 
+
     if args.verbose:
-        # Add another logging handler to get output to console
-        handler_stream = logging.StreamHandler(sys.stdout)
-        handler_stream.setFormatter(formatter)
-        handler_stream.setLevel(logging.DEBUG)
-        logger.addHandler(handler_stream)
+        # Logging to console if verbose flag is present
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(formatter)
         logger.setLevel(logging.DEBUG)
     else:
-        # Just logs this level of messages
+        # Othewise logs to file
+        handler = logging.handlers.RotatingFileHandler(
+                      LOG_FILENAME, maxBytes=104857600, backupCount=2)
+        handler.setFormatter(formatter)
         logger.setLevel(logging.INFO)
+    
+    logger.addHandler(handler)
 
-    args.func(args)
+    # Init new soap client
+    client = Client(args.wsdl)
+    # run appropriate function
+    args.func(client, args)
+
     
     
 if __name__ == '__main__':
