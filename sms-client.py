90fa399ca1a823b08c180dacee946781b8d3420b
Bushuev Alexandr <a.bushuev@roseurobank.ru>
2018-12-18 18:05:23 +0300

    tested and working ver

--- sms-client.py
+++ sms-client.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 import sys
 import os
 import argparse
 import logging
 import logging.handlers
-from uuid import uuid4
 from suds.client import Client
+from uuid import uuid4
 
-
-
-LOG_FILENAME = '/tmp/sms.log'
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 def handle_exception(exc_type, exc_value, exc_traceback):
@@ -27,21 +27,54 @@ def handle_exception(exc_type, exc_value, exc_traceback):
 sys.excepthook = handle_exception
 
 
-# subcommand functions
+def main():
 
-# Send message
-def send(client, auth, args):
-    """ Try to send message over sms gateway """
+    # Local working WSDL because requested from server has wrong soap address location
+    url = 'file:///'+ os.getcwd() + '/OutMessageService.wsdl.prod'
+    wsdl_local = url
+
+    parser = argparse.ArgumentParser(description='Simple soap client for sending sms over MFM Sms gateway')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-v", "--verbose", action="store_true",
+            help="Verbose output")
+    group.add_argument("-q", "--quiet", action="store_true", default=True,
+            help="Suppres output. Default")
+
+    parser.add_argument("phone",
+            help="Sender phone number", type=str)
+    parser.add_argument("message",
+            help="Message to send", type=str)
+
+    parser.add_argument("-u", "--user", help="Smsgate user login",
+            action="store", type=str, default='ws')
+    parser.add_argument("-p", "--password", help="Smsgate user password",
+                        action="store", type=str, default='ws')
+    parser.add_argument("-s", "--sender",
+            help="Sender name. Displayed in sms header. Must be set according provider rules. Default: Sovcombank",
+                        action="store", default='Sovcombank')
+    parser.add_argument("-t", "--type",
+            help="Sender type outMessageTypeId. Sender type needed for sms gate",
+                        action="store", default='sys_monitoring')
+    parser.add_argument("-w", "--wsdl",
+            help="Sms gate wsdl url",
+            action="store_true", default=wsdl_local)
+
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.ERROR)
 
     logger.debug("Connecting with those params: Wsdl url: %s, "
                   "Sender name: %s, Sender type: %s, User: %s, "
                   "Pass %s, Sending message to: %s With content: %s",
     args.wsdl, args.sender, args.type, args.user, args.password, args.phone, args.message)
 
-    # Write messageid to logfile allways level info
-    messageId = uuid4()
-    logger.info("Sending message with ID: %s To: %s", messageId, args.phone)
-    logger.debug("Client: %s", client)
+    # Sms client implemetation
+    client = Client(args.wsdl)
+    logger.debug(client)
 
     auth = client.factory.create('ns0:Auth')
     auth.login = args.user
@@ -69,100 +102,6 @@ def send(client, auth, args):
     logger.debug(send_status)
 
 
-# Get message status
-def get_status(client, auth, args):
-    """ Get status of previously sended message """
-
-    logger.debug("Trying to get status for message: %s", args.messageId)
-    getOutMessageDlvStatusArg = client.factory.create('ns0:GetOutMessageDlvStatusArg')
-    getOutMessageDlvStatusArg.messageId = args.messageId
-
-    message_status = client.service.getOutMessageDlvStatus(auth=auth, getOutMessageDlvStatusArg=getOutMessageDlvStatusArg)
-    message_status = dict(message_status)
-    logger.info("Delivery responseCode: %s with status: %s", \
-                message_status['responseCode'], \
-                message_status['getOutMessageDlvStatusResult'][0]['outMessageDlvStatus']['dlvStatus'])
-
-
-
-def main():
-
-    parser = argparse.ArgumentParser(
-            description='Simple soap client for sending sms over MFM Sms gateway. \
-                    Also it cat get status for previously sended sms')
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-v", "--verbose", action="store_true",
-            help="Verbose output")
-    group.add_argument("-q", "--quiet", action="store_true", default=True,
-            help="Suppres output. Default")
-    parser.add_argument("-u", "--user", help="Smsgate user login",
-            action="store", type=str)
-    parser.add_argument("-p", "--password", help="Smsgate user password",
-                        action="store", type=str)
-    parser.add_argument("-w", "--wsdl",
-            help="Sms gate wsdl url or file.",
-            action="store", default='http://sms-gate/sendmessage.wsdl')
-    
-    subparsers = parser.add_subparsers(help='sub-command help')
-
-
-    get_parser = subparsers.add_parser('get_status', help='Returns delivery status for sms based on messageId')
-    get_parser.add_argument("messageId", help="Message ID to look for")
-    get_parser.set_defaults(func=get_status)
-
-
-    send_parser = subparsers.add_parser('send', help='Send message to sms gateway')
-    send_parser.add_argument("phone",
-            help="Sender phone number", type=str)
-    send_parser.add_argument("message",
-            help="Message to send", type=str)
-    send_parser.add_argument("-s", "--sender",
-            help="Sender name. Displayed in sms header. Must be set according provider rules. Default: Sender",
-                        action="store", default='Sender')
-    send_parser.add_argument("-t", "--type",
-            help="Sender type. Sender type needed for sms gate",
-                        action="store", default='senderType')
-    send_parser.set_defaults(func=send)
-
-
-    args = parser.parse_args()
-
-
-    if args.verbose:
-        # Logging to console if verbose flag is present
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(formatter)
-        logger.setLevel(logging.DEBUG)
-    else:
-        # Othewise logs to file
-        handler = logging.handlers.RotatingFileHandler(
-                      LOG_FILENAME, maxBytes=104857600, backupCount=2)
-        handler.setFormatter(formatter)
-        logger.setLevel(logging.INFO)
-    
-    logger.addHandler(handler)
-
-    #Constuct wsdl url
-    if args.wsdl.split(':')[0] in ['https', 'http']:
-        # use as it is
-        pass
-    else:
-        # local file
-        args.wsdl = 'file://' + os.path.abspath(args.wsdl)
-
-    # Init new soap client
-    client = Client(args.wsdl)
-
-    # Add auth info
-    auth = client.factory.create('ns0:Auth')
-    auth.login = args.user
-    auth.password = args.password
-
-    # run appropriate function
-    args.func(client, auth, args)
-
-    
     
 if __name__ == '__main__':
     main()
-
